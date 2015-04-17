1df894be0366eb7e6b5e72494a546fe893b4567f
Filip Procházka <filip@prochazka.su>
2015-04-17 20:59:39 +0200

    Fixed exception code range

--- src/Kdyby/Console/Application.php
+++ src/Kdyby/Console/Application.php
@@ -108,7 +108,7 @@ class Application extends Symfony\Component\Console\Application
 				$this->handleException($e, $output);
 			}
 
-			return max(min((int) $e->getCode(), 254), 254);
+			return max(min((int) $e->getCode(), 254), 1);
 		}
 	}
 
