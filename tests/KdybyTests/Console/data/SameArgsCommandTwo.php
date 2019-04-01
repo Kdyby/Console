92b4ac6346fe136a204c8e5aa1ef74bf0165f958
VBoss <vcevela@gmail.com>
2019-04-02 00:17:46 +0200

    unused arguments in PHPStan - so use them as if it was application - added to properties and made getters

--- tests/KdybyTests/Console/data/SameArgsCommandTwo.php
+++ tests/KdybyTests/Console/data/SameArgsCommandTwo.php
@@ -11,9 +11,21 @@ class SameArgsCommandTwo extends \Symfony\Component\Console\Command\Command
 
 	use \Kdyby\StrictObjects\Scream;
 
+	/**
+	 * @var \KdybyTests\Console\ArgCommand
+	 */
+	private $argCommand;
+	/**
+	 * @var \KdybyTests\Console\TypoCommand
+	 */
+	private $typoCommand;
+
+
 	public function __construct(ArgCommand $argCommand, TypoCommand $typoCommand)
 	{
 		parent::__construct();
+		$this->argCommand = $argCommand;
+		$this->typoCommand = $typoCommand;
 	}
 
 	protected function configure()
@@ -24,6 +36,18 @@ class SameArgsCommandTwo extends \Symfony\Component\Console\Command\Command
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
 		Assert::fail("This command shouldn't have been executed.");
+	}
+
+
+	public function argCommand() : \KdybyTests\Console\ArgCommand
+	{
+		return $this->argCommand;
+	}
+
+
+	public function typoCommand() : \KdybyTests\Console\TypoCommand
+	{
+		return $this->typoCommand;
 	}
 
 }
